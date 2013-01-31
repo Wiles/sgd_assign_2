650a10046895915e2c1500ed149a964333fb2c12
Samuel Lewis <samuelewis@gmail.com>
2013-01-31 08:08:10 -0500

    Clean Pendulum project

---
+++ Pendulum/Pendulum/Pendulum/Program.cs
@@ -0,0 +1,19 @@
+namespace Pendulum
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (var game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
