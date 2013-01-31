6a789d9db1aecf16907cfa4898f7ee80f6eaa308
Samuel Lewis <samuelewis@gmail.com>
2013-01-31 08:07:54 -0500

    Dice change

--- Dice/Dice/Form1.Designer.cs
+++ Dice/Dice/Form1.Designer.cs
@@ -28,11 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.nub_range = new System.Windows.Forms.NumericUpDown();
             this.btn_run = new System.Windows.Forms.Button();
             this.results = new System.Windows.Forms.DataVisualization.Charting.Chart();
@@ -79,24 +79,24 @@
             this.results.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            chartArea1.Name = "ChartArea1";
-            this.results.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.results.Legends.Add(legend1);
+            chartArea2.Name = "ChartArea1";
+            this.results.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.results.Legends.Add(legend2);
             this.results.Location = new System.Drawing.Point(12, 42);
             this.results.Name = "results";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Min";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Actual";
-            series3.ChartArea = "ChartArea1";
-            series3.Legend = "Legend1";
-            series3.Name = "Max";
-            this.results.Series.Add(series1);
-            this.results.Series.Add(series2);
-            this.results.Series.Add(series3);
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Min";
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Actual";
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Max";
+            this.results.Series.Add(series4);
+            this.results.Series.Add(series5);
+            this.results.Series.Add(series6);
             this.results.Size = new System.Drawing.Size(664, 482);
             this.results.TabIndex = 3;
             this.results.Text = "chart1";
