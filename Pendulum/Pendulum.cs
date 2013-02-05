716ca838bbf4018918a0a7991aceecd6402e0b1b
Samuel Lewis <samuelewis@gmail.com>
2013-02-04 20:21:47 -0500

    Scaling and volume

--- Pendulum/Pendulum.cs
+++ Pendulum/Pendulum.cs
@@ -14,7 +14,7 @@ namespace Pend
         private readonly SecondaryBuffer _sound;
         private readonly int _sWidth;
         private const double MaxAngle = .5;
-        private const double Length = 1;
+        private const float Length = 10;
 
         public Pendulum(Direct3D.Device g, Size gSize, Direct3D.Texture texture, SecondaryBuffer sound)
         {
@@ -31,14 +31,20 @@ namespace Pend
             var rotation = (float) (MaxAngle*Math.Cos(Math.Sqrt(9.81/Length)*timer.Time()));
             _sprite.Begin(Direct3D.SpriteFlags.AlphaBlend);
             Matrix tranz = 
-            Matrix.Transformation2D(new Vector2(0.0f, 0.0f), 0.0f,new Vector2(1.0f, 1.0f),
-                new Vector2((float) (_sWidth/2.0), 0), rotation, new Vector2(0.0f, 0.0f));
+            Matrix.Transformation2D(
+                new Vector2((float)(_sWidth / 2.0), 0.0f),          //scaling centre
+                0.0f,                                               //scaling ratio
+                new Vector2(Length / 5.0f, Length / 5.0f),          //scaling
+                new Vector2((float)(_sWidth / 2.0), 0),             //rotation centre
+                rotation,                                           //rotation
+                new Vector2(0.0f, 0.0f));                           //translation
 
             _sprite.Transform = tranz;
             _sprite.Draw(_texture, Rectangle.Empty, new Vector3(50, 0.0f, 0.0f), new Vector3((float) (_sWidth/2.0), 0, 0.0f), Color.White);
             _sprite.End();
 
             _sound.Pan = -(int) (10000 * (float)((rotation / MaxAngle) * .5));
+            _sound.Volume = (int) (-1000 * (Math.Abs(rotation)/(MaxAngle)));
         }
     }
 }
